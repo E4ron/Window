ebda40e660b9e864aca79d423dbe66a25b81a406
student student <student@Kurgantehkolledg.onmicrosoft.com>
2021-09-25 08:39:14 +0500

    change style

---
+++ NEwProdgekt/go.mod
@@ -0,0 +1,3 @@
+module NEwProdgekt
+
+go 1.17
---
+++ NEwProdgekt/script.js
@@ -0,0 +1,54 @@
+let buttonCreateWindow = document.querySelectorAll("#ButtonCreateWindow");
+if (buttonCreateWindow) {
+    buttonCreateWindow.onclick = createWindow
+}
+let zIndex = 1;
+/**
+* @param {MouseEvent} event
+*/
+function createWindow (event) {
+    let rootWindow = document.createElement("div")
+    head = document.createElement("div"),
+        body = document.createElement("div"),
+        text = document.createElement("div"),
+        cluse = document.createElement("div");
+    rootWindow.append(head, body);
+    head.append(text, cluse);
+    head.onmousedown = onmousedown;
+    head.onmousemove = onmousemove;
+    head.onmouseup = onmouseup;
+    text.textContent = "This window";
+    rootWindow.className = "Window";
+    head.className = "head";
+    body.className = "body";
+    cluse.className = "cluse";
+}
+function closeWindow() {
+    let thisWindow = this.closeWindow("Window");
+    if (thisWindow) {
+        thisWindow.remove();
+    }
+}
+/**
+@param {MouseEvent} event
+ */
+function onMouseDown(event){
+    this.dataset.isMove ="true";
+    this.dataset.x = "" + event.clientX;
+    this.dataset.y = "" + event.clientY;
+    this.parameterElement.style.zIndex = "" + (zIndex++)
+
+}
+/**
+ @param {MouseEvent} event
+ */
+function onMouseUp(event) {
+    this.dataset.1sMove = "false";
+}
+/**
+ @param {MouseEvent} event
+ */
+function onMouseMove (event) {
+if(this.parameter)
+}
+
